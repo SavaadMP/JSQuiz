d68ba4172f5452acb97716b95dd4350cfdcdae7e
MuhammedSavaad <savadmoorkkanad@gmail.com>
2023-02-18 14:23:32 +0530

    Random Question

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var Questions = [
   {
+    Qno: "1",
     Que: "Who Is The Father Of Politcal Science!?",
     a: "Augaste Comte",
     b: "Aristotle",
@@ -8,6 +9,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "2",
     Que: '"War is to man what maternity is to women" Whose Word It is?',
     a: "Adolf Hitlur",
     b: "John Lockie",
@@ -16,6 +18,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "3",
     Que: "Who Signed Panchasheel Principle With China!?",
     a: "Augaste Comte",
     b: "Jawaharlal Nehru",
@@ -24,6 +27,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "4",
     Que: "Who Is The Father Of Sociology!?",
     a: "Augaste Comte",
     b: "Aristotle",
@@ -32,6 +36,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "5",
     Que: "Who Is The Father Of Genetics!?",
     a: "Alexander Fleming",
     b: "Johns Mendal",
@@ -40,6 +45,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "6",
     Que: "Which Nation is not included in TRIPLE ALLIANCE!?",
     a: "Germany",
     b: "Italy",
@@ -48,6 +54,7 @@ var Questions = [
     answer: "d",
   },
   {
+    Qno: "7",
     Que: "Which Nation is not included in ALLIED POWERS!?",
     a: "England",
     b: "France",
@@ -56,6 +63,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "8",
     Que: "Find Out The Wrong Pair.",
     a: "Champaran Sathyagraha - 1916",
     b: "Lahore Session of INC - 1929",
@@ -64,6 +72,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "9",
     Que: "Industrial Production of Soap Is Known as",
     a: "Contact Process",
     b: "Hot Process",
@@ -72,6 +81,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "10",
     Que: "Least distance of distinct vision for a normal human being is .......",
     a: "20m",
     b: "25m",
@@ -80,6 +90,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "12",
     Que: "Which among the following factors has no effect on seasons?",
     a: "Earth's revolution",
     b: "Axis of earth",
@@ -88,6 +99,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "13",
     Que: "Which longitude is known as primemeridian?",
     a: "0",
     b: "180",
@@ -96,6 +108,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "14",
     Que: "Which Geographic Information System is used to understand the various features on earth's surface and its periodic changes?",
     a: "Remote Sensing",
     b: "Overlay Analysis",
@@ -104,6 +117,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "15",
     Que: "Select the wrong statement related to Tungsten filament?",
     a: "High Resistivity",
     b: "High ductility",
@@ -112,6 +126,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "16",
     Que: "If size of the image formed by convex lens as same as the size of the object, then what is the magnification?",
     a: "+1",
     b: "-2",
@@ -120,6 +135,7 @@ var Questions = [
     answer: "-1",
   },
   {
+    Qno: "17",
     Que: "Which defect of vision can be rectified by using concave lens.",
     a: "Near sightedness",
     b: "Longsightedness",
@@ -128,6 +144,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "18",
     Que: "Potential difference between 2 phase line is.....",
     a: "11KV",
     b: "11V",
@@ -136,6 +153,7 @@ var Questions = [
     answer: "d",
   },
   {
+    Qno: "19",
     Que: "Name the vaccine against Tuberculosis.",
     a: "MMR",
     b: "BCG",
@@ -144,6 +162,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "20",
     Que: "Which RNA transfers the message from DNA?",
     a: "mRNA",
     b: "tRNA",
@@ -152,6 +171,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "21",
     Que: "Which part of the brain maintains balance of the body",
     a: "cerebrum",
     b: "cerebellum",
@@ -160,6 +180,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "22",
     Que: "Who proposed mutation theory?",
     a: "Charles Darwin",
     b: "Robert Malthus",
@@ -168,6 +189,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "23",
     Que: "Who invented DNA finger printing",
     a: "Alec Jaffrey",
     b: "Alexander Fleming",
@@ -176,6 +198,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "24",
     Que: "Which one of the following subshells is not possible in an atom?",
     a: "1s",
     b: "2p",
@@ -184,6 +207,7 @@ var Questions = [
     answer: "d",
   },
   {
+    Qno: "25",
     Que: "Calamine is the ore of ............",
     a: "Zinc",
     b: "Alumnium",
@@ -192,6 +216,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "26",
     Que: "What is the energy change in Galvanic Cell?",
     a: "Chemichal -> Electrical",
     b: "Electrical -> Chemichal",
@@ -200,6 +225,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "27",
     Que: "In which block lanthanoid and actinoids belong?",
     a: "s block",
     b: "d block",
@@ -208,6 +234,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "28",
     Que: "Name the industrial preparation of NH3?",
     a: "Hot process",
     b: "contact process",
@@ -216,6 +243,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "29",
     Que: "The homeopath ........(a)......... at the police station to give a complaint.",
     a: "went on",
     b: "turned up",
@@ -224,6 +252,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "30",
     Que: "she (is) surprised to see a (beautiful) woven basket made by his brother.",
     a: "was, beautiful",
     b: "is, beautifully",
@@ -232,6 +261,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "31",
     Que: "Name the quadrilateral for which we can always draw incircle",
     a: "Parallelogram",
     b: "rectangle",
@@ -240,6 +270,7 @@ var Questions = [
     answer: "d",
   },
   {
+    Qno: "32",
     Que: "The algebraic form of an arithmetic sequence is 4n-3. What is the common difference?",
     a: "4",
     b: "-4",
@@ -248,6 +279,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "33",
     Que: "Name the social reform movement led by V.T Bhattathiripad.",
     a: "Admavidhyasangham",
     b: "Sadhu Jana Paripalana Sangham",
@@ -256,6 +288,7 @@ var Questions = [
     answer: "d",
   },
   {
+    Qno: "34",
     Que: "Co-operation, self help and mutual help are the principles of which bank?",
     a: "Commercial Bank",
     b: "Reserve Bank",
@@ -264,6 +297,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "35",
     Que: "Which local wind blows through the North Indian plains?",
     a: "Chinook",
     b: "Loo",
@@ -272,6 +306,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "36",
     Que: "In which state is Koodamkulam nuclear power plant situated?",
     a: "Tamil Nadu",
     b: "Karnataka",
@@ -280,6 +315,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "37",
     Que: "Which artificial satellite passes around the earth along the poles?",
     a: "GIS",
     b: "GPS",
@@ -288,6 +324,7 @@ var Questions = [
     answer: "d",
   },
   {
+    Qno: "38",
     Que: "The day in which the sun is vertically above the tropic of capricorn is called?",
     a: "Winter solstice",
     b: "Summer solstice",
@@ -296,6 +333,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "39",
     Que: "Which Indian city is termed as 'Cottonopolis'?",
     a: "Kandla",
     b: "Kolatha",
@@ -304,6 +342,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "40",
     Que: "Which is considered as the biggest port of India ?",
     a: "mumbai",
     b: "kolkatha",
@@ -312,6 +351,7 @@ var Questions = [
     answer: "a",
   },
   {
+    Qno: "41",
     Que: "Name the Governor General who abolished sati in 1829 ?",
     a: "Lord Clive",
     b: "Lord Curzon",
@@ -320,6 +360,7 @@ var Questions = [
     answer: "c",
   },
   {
+    Qno: "42",
     Que: " The hottest planet in the solar system ?",
     a: "Earth",
     b: "venus",
@@ -328,6 +369,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "43",
     Que: "One People, One State, One leader” was the policy of ?",
     a: "Stalin",
     b: "Hitler",
@@ -336,6 +378,7 @@ var Questions = [
     answer: "b",
   },
   {
+    Qno: "44",
     Que: "The first computer made available for commercial use was",
     a: "MANIAC",
     b: "ENIAC",
@@ -358,6 +401,8 @@ let index = Math.floor(Math.random() * Questions.length - 1 + 1);
 let QueTitle = document.getElementById("QueTitle");
 // Recieving The Answer From User;
 let userOutput = document.querySelectorAll(".options");
+
+let randomQns=[];
 
 // Reset The Answer After CLicking Oneeee;
 const reset = () => {
@@ -397,6 +442,7 @@ const AnswerInput = () => {
 
 // Quizz Submitting......
 const submitQuiz = () => {
+  randomQns.push(index);
   // Getting The Qustions Array
   const data = Questions[index];
   // Getting The  Answer
@@ -414,9 +460,12 @@ const submitQuiz = () => {
     // Changing The Question..
     QNumber++;
     index = Math.floor(Math.random() * Questions.length - 1 + 1);
+    while(randomQns.indexOf(index)!==-1){
+      index = Math.floor(Math.random() * Questions.length - 1 + 1);
+    }
     loadQuestion(index);
+    randomQns.push(index);
   }
-
   return;
 };
 
